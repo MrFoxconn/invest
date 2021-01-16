81a91bfef5284ac5b29c7927026e5dd50a700c72
Mantis-3DP <mantis3dp@posteo.de>
2021-01-16 14:45:09 +0100

    first

---
+++ runme.py
@@ -0,0 +1,34 @@
+# import yfinance as yf
+#
+#
+#
+# data = yf.download("SPY AAPL", start="2017-01-01", end="2017-04-30")
+#
+# print(data)
+
+
+
+
+class investment:
+
+    def __init__(self, maxGeschoss, aGrund, GFZ):
+
+        self.maxGeschoss = maxGeschoss
+        self.aGrund = aGrund
+        self.GFZ = GFZ
+
+
+
+        pass
+
+
+    def rentPrice(self, N_aprt, Qm_aprt, count_room):
+        pass
+
+
+
+    def aWohn(self):
+        maxA = self.aGrund * self.GFZ
+
+
+        return maxA
